6c8c345a96b9b6c2a967665498482fb35d6df2bc
Ahmed Essam <70452432+A7med-Essam@users.noreply.github.com>
2023-03-19 19:59:29 +0200

    translate not found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent implements OnInit {
-
+export class NotFoundComponent implements OnInit, OnDestroy {
   constructor(
-    private _Router:Router
-  ) { }
+    private _Router: Router,
+    private _TranslateService: TranslateService
+  ) {}
+  private unsubscribe$ = new Subject<void>();
 
+  lang: string = '';
   ngOnInit(): void {
+    this.lang = this._TranslateService.currentLang
+    this._TranslateService.onLangChange
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (res: any) => {
+          this.lang = this._TranslateService.currentLang;
+        },
+      });
   }
 
-  getHomePage(){
-    this._Router.navigate(['home'])
+  getHomePage() {
+    this._Router.navigate(['home']);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
